b9ad8a324e675efb865f60359477377f5955f633
Harshavardhan0306 <aharsha0306@gmail.com>
2026-02-12 16:09:01 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+/*
+*uc 1 palindrome checker
+*                   */
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("welcome to palindrome manager system\nversion :1.0\n system initialized sucessfully ");
+    }
+}
